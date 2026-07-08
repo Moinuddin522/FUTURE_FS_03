420db5e028d4b182f77e76d8601a2c76f76e471d
Moinuddin522 <Mohammadayan7765@gmail.com>
2026-07-08 23:59:08 +0530

    feat: add contact and appointment section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import MainLayout from "../layouts/MainLayout";
-
 import Hero from "../components/Hero/Hero";
 import Features from "../components/Features/Features";
 import Services from "../components/Services/Services";
 import Testimonials from "../components/Testimonials/Testimonials";
 import Gallery from "../components/Gallery/Gallery";
+import ContactSection from "../components/ContactSection/ContactSection";
 
 const Home = () => {
   return (
@@ -14,6 +14,7 @@ const Home = () => {
       <Services />
       <Testimonials />
       <Gallery />
+      <ContactSection />
     </MainLayout>
   );
 };
